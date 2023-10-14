9161a054e8f50365e829911a55a730ee5d21946b
HanguelMansae <111sym@naver.com>
2023-10-14 14:52:48 +0900

    add hourchart comment and delete unusable code

--- Cool_Time/app/src/main/java/com/example/cool_time/utils/ChartHourFragment.kt
+++ Cool_Time/app/src/main/java/com/example/cool_time/utils/ChartHourFragment.kt
@@ -1,17 +1,13 @@
 package com.example.cool_time.utils
 
-import android.content.Context
 import android.content.pm.PackageManager
-import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Path
-import android.graphics.drawable.GradientDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.graphics.drawable.toBitmap
 import androidx.fragment.app.Fragment
 import com.example.cool_time.databinding.FragmentHourChartBinding
 import com.github.mikephil.charting.animation.ChartAnimator
@@ -26,23 +22,28 @@ import com.github.mikephil.charting.data.BarDataSet
 import com.github.mikephil.charting.data.BarEntry
 import com.github.mikephil.charting.interfaces.datasets.IBarDataSet
 import com.github.mikephil.charting.renderer.BarChartRenderer
-import com.github.mikephil.charting.utils.Utils
 import com.github.mikephil.charting.utils.ViewPortHandler
 
 
+// 직접 차트 커스텀 하기 위해 BarChartRenderer 상속 받아서 설정
+class BarChartRender(aChart: BarChart, aAnimator: ChartAnimator,
+                     aViewPortHandler: ViewPortHandler,
 
-class BarChartRenderer(aChart: BarChart, aAnimator: ChartAnimator,
-                                     aViewPortHandler: ViewPortHandler, aImageList: ArrayList<Bitmap>,
-                                     aContext: Context
-)
+                     )
     : BarChartRenderer(aChart, aAnimator, aViewPortHandler) {
-    private val mRadius = 30F; //둥글게 설정할 크기 설정
-    private fun roundedRect( //상하좌우 위치 받고, 네 모서리의 round 유무를 bool 값으로 받아서 round 처리해줌
+    //둥글게 설정할 크기 설정
+    private val mRadius = 30F
+
+    //상하좌우 위치 받고, 네 모서리의 round 유무를 bool 값으로 받아서 round 처리해줌
+    private fun roundedRect(
         left: Float, top: Float, right: Float, bottom: Float, rx: Float, ry: Float,
         tl: Boolean, tr: Boolean, br: Boolean, bl: Boolean
     ): Path {
-        var rxValue = rx //x축 round 할 크기 설정
-        var ryValue = ry //x축 round 할 크기 설정 ==> rxvalue는 원의 x축 반지름, ryvalue는 원의 y축 반지름이라고 생각하면 됨 (각 값이 다르면 타원 같은 느낌)
+        // rxvalue는 원의 x축 반지름, ryvalue는 원의 y축 반지름이라고 생각하면 됨 (각 값이 다르면 타원 같은 느낌)
+        //x축 round 할 크기 설정
+        var rxValue = rx
+        //y축 round 할 크기 설정
+        var ryValue = ry
 
         //음수로 들어오면 그냥 없는 거로 설정
         if (rxValue < 0) rxValue = 0f
@@ -60,9 +61,12 @@ class BarChartRenderer(aChart: BarChart, aAnimator: ChartAnimator,
             ryValue = height / 2; rxValue = ryValue
         }
 
-        val widthMinusCorners = (width - (2 * rxValue))
+        //val widthMinusCorners = (width - (2 * rxValue))
+        // 곡선을 제외한 x축으로부터 수직인 바의 길이
         val heightMinusCorners = (height - (2 * ryValue))
-        val path = Path() //안드로이드에서 그래픽을 그리고 경로를 정의하는데 사용되는 클래스
+
+        // 안드로이드에서 그래픽을 그리고 경로를 정의하는데 사용되는 클래스
+        val path = Path()
         /* 참고사항 : path관련 상대좌표를 매개변수로 받는 함수에서
                     y인자를 음수로 설정하면 막대바의 위치가 오히려 더 위로 올라감, 양수를 줘야지 아래로 내려감,
                     x값은 음수면 왼쪽, 플러스면 오른쪽으로 이동함
@@ -114,8 +118,10 @@ class BarChartRenderer(aChart: BarChart, aAnimator: ChartAnimator,
             path.rLineTo(0f, -ryValue)
         }
 
-        path.rLineTo(0f, -heightMinusCorners) // 아까와 마찬가지로 이번엔 남은 우측 변의 길이만큼 그려줌
-        path.close() // Given close, last lineto can be removed. // 다 그렸으니까 종료 처리
+        // 아까와 마찬가지로 이번엔 남은 우측 변의 길이만큼 그려줌
+        path.rLineTo(0f, -heightMinusCorners)
+        // 다 그렸으니까 종료 처리
+        path.close() // Given close, last lineto can be removed.
         return path
     }
 
@@ -128,7 +134,9 @@ class BarChartRenderer(aChart: BarChart, aAnimator: ChartAnimator,
         if (mBarBuffers != null) {
             // Initialize the buffer
             val buffer = mBarBuffers[index] // mBarBuffers는 막대 바를 가지는 array로 판단됨
-            buffer.setPhases(phaseX, phaseY) // 해당 막대의 애니메이션 진행도를 설정 ex: 막대바 길이가 100이고 phase가 0.5로 설정되어 있으면 50은 이미 그려진 상태에서 애니메이션 시작
+            // 해당 막대의 애니메이션 진행도를 설정
+            // ex: 막대바 길이가 100이고 phase가 0.5로 설정되어 있으면 50은 이미 그려진 상태에서 애니메이션 시작
+            buffer.setPhases(phaseX, phaseY)
             //barWidth 등
             buffer.setBarWidth(mChart.barData.barWidth)
             buffer.setInverted(mChart.isInverted(dataSet.axisDependency))
@@ -138,7 +146,8 @@ class BarChartRenderer(aChart: BarChart, aAnimator: ChartAnimator,
             // If multiple colors
             if (dataSet.colors.size > 1) {
                 var j = 0
-                while (j < buffer.size()) {//막대가 뷰포트(화면에 표시된 차트 영역)내에서 좌측 경계, 우측 경계 내에 있는지를 확인해서 조건 처리
+                //막대가 뷰포트(화면에 표시된 차트 영역)내에서 좌측 경계, 우측 경계 내에 있는지를 확인해서 조건 처리
+                while (j < buffer.size()) {
                     if (!mViewPortHandler.isInBoundsLeft(buffer.buffer[j + 2])) {
                         continue
                     }
@@ -225,7 +234,7 @@ class BarChartRenderer(aChart: BarChart, aAnimator: ChartAnimator,
                             )
                         }
                     }
-                    if (mRadius > 0) { //컬러도 한개고 그림자 설정 안하고 굴골 설정했을때
+                    if (mRadius > 0) { //컬러도 한개고 그림자 설정 안하고 굴곡 설정했을때 -- 우리차트는 이 조건식에 부합해 이부분의 코드 수정
                         val path = roundedRect(
                             buffer.buffer[j],
                             buffer.buffer[j + 1],
@@ -252,8 +261,8 @@ class BarChartRenderer(aChart: BarChart, aAnimator: ChartAnimator,
 }
 class ChartHourFragment : Fragment(){
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        var binding = FragmentHourChartBinding.inflate(inflater, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        val binding = FragmentHourChartBinding.inflate(inflater, container, false)
         initBarChart(binding.chart)
         setData(binding.chart)
         return binding.root
@@ -334,65 +343,68 @@ class ChartHourFragment : Fragment(){
             setTouchEnabled(false)}
     }
 
+    // 앱이 스마트폰 내에 설치되어 있는지 확인
+    private fun isAppInstalled(packageName : String, packageManager : PackageManager) : Boolean{
+        return try{
+            packageManager.getPackageInfo(packageName, 0) // 있으면 true
+            true
+        }catch (ex : PackageManager.NameNotFoundException){ // 없으면 false
+            false
+        }
+    }
+
     // 차트 데이터 설정
     private fun setData(barChart: BarChart) {
 
         // Zoom In / Out 가능 여부 설정
         barChart.setScaleEnabled(false)
 
-        val valueList = ArrayList<BarEntry>()
-        val title = "걸음 수"
-        var map = ArrayList<Bitmap>()
-        // 임의 데이터
+        val valueList = ArrayList<BarEntry>() // 하나의 막대 바 만드는데 필요한 값들을 넣는 BarEntry들을 담을 리스트 생성
+        val title = "차트" // 어차피 안보이게 할거라 필요는 없지만 차트 이름 설정
+
+        // 임의 수동 반복문 데이터
         for (i in 0 until 24) {
-            val packageName = "com.kakao.talk"
-            fun isAppInstalled(packageName : String, packageManager : PackageManager) : Boolean{
-                return try{
-                    packageManager.getPackageInfo(packageName, 0)
-                    true
-                }catch (ex : PackageManager.NameNotFoundException){
-                    false
-                }
+            //테스트 위해서 그냥 임의로 큰값 한번 넣어줌
+            if(i == 3) {
+                valueList.add(BarEntry(i.toFloat(), i * 1000f,))
             }
-
-
-            val pm :PackageManager = this.activity!!.packageManager
-            val a = pm.getApplicationIcon(packageName)
-
-            if(isAppInstalled(packageName, pm)){
-                map.add(a.toBitmap())
-                if(i == 3) {
-                    valueList.add(BarEntry(i.toFloat(), i * 1000f,))
-                }
-                else{
-                    valueList.add(BarEntry(i.toFloat(), i * 100f,))
-                }
+            else{
+                valueList.add(BarEntry(i.toFloat(), i * 100f,))
             }
-
-
-
-
         }
-
+        // 막대바들의 정보를 담은 BarEntry List와 차트 이름을 넣어서 하나의 차트를 만드는데 필요한 DataSet 생성
         val barDataSet = BarDataSet(valueList, title)
-        val drawable = GradientDrawable()
-        barChart.setPadding(0,0,0,0)
-        // 바 색상 설정 (ColorTemplate.LIBERTY_COLORS)
+
+        // 막대바 위에 값을 출력해줌
         barDataSet.setDrawValues(false)
+
+        // 값 출력해줄 때 텍스트의 크기 설정하는건데 false로 설정했으니까 애초에 필요 X
+        //data.setValueTextSize(Utils.convertDpToPixel(5F))
+
         barDataSet.setColors(
+            // 막대바 색깔 설정 => 여러개 넣으면 순서대로 나뉘어서 설정
             Color.rgb(238, 71, 77)
         )
 
-
-
-
+        // DataSet들을 하나의 BarData로 초기화
+        // 만약 여러 DataSet들을 넘겨준다면 여러 차트가 하나의 X축과 Y축을 바탕으로 하는 화면에 같이 그려짐
         val data = BarData(barDataSet)
+
+        // 상대적인 bar 두께 설정함 ==> ★ 리사이클러 뷰마냥 유동적으로 차트 크기 설정하는게 아닌, 상위 레이아웃의 크기에 맞춰서 차트가 구현됨
+        // => 막대 두께도 상위 레이아웃 크기에 따라 달라짐
         data.barWidth = 0.3f
-        data.setValueTextSize(Utils.convertDpToPixel(5F))
+
+        //차트의 데이터 바인딩, 세팅된 데이터 값을 차트에 넣어줌
         barChart.data = data
-        var ca = barChart.animator
-        var vp = barChart.viewPortHandler
-        barChart.renderer = BarChartRenderer(barChart, ca, vp, map, context!!)
-        barChart.invalidate()
+
+        // 차트 데이터 그릴 때 설정할 애니메이션 효과 정보 담겨있는 변수
+        val animator = barChart.animator
+
+        // 차트의 화면 영역, 확대 축소 등을 담당하는데 width와 height 조절이 불가능 set 함수가 없음....
+        val viewPortHandler = barChart.viewPortHandler
+
+        //rendering 어떻게 할 건지 설정 => 커스텀 하려면 직접 그려야 해서 필요
+        barChart.renderer = BarChartRender(barChart, animator, viewPortHandler)
+        barChart.invalidate() // 차트를 새로 그리는 메소드
     }
 }
