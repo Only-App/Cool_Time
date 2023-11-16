745d16772ac153c7b7002688fc5cb1ce15581f58
HanguelMansae <111sym@naver.com>
2023-11-17 02:18:40 +0900

    init KakaoSharingSDK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,7 +59,9 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.navigation:navigation-fragment-ktx:2.4.1")
     implementation("androidx.navigation:navigation-ui-ktx:2.4.1")
+    implementation("com.kakao.sdk:v2-share:2.18.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
 }
--- app/src/main/java/com/onlyapp/cooltime/MyApplication.kt
+++ app/src/main/java/com/onlyapp/cooltime/MyApplication.kt
@@ -6,10 +6,14 @@ import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.os.Build
+import android.util.Log
 import androidx.annotation.RequiresApi
+import com.kakao.sdk.common.KakaoSdk
+import com.kakao.sdk.common.util.Utility
 import com.onlyapp.cooltime.data.DataStoreModule
 import com.onlyapp.cooltime.receiver.MyBroadcastReceiver
 
+const val NATIVE_APP_KEY = "92226dfd8157cb8b6a20c3f518c39d06" // 안드로이드에서 KakaoSdk 호출 위한 네이티브 key 값
 class MyApplication : Application() {
     private lateinit var dataStore : DataStoreModule
 
@@ -24,6 +28,14 @@ class MyApplication : Application() {
             addAction(Intent.ACTION_SCREEN_OFF)
             addAction(Intent.ACTION_DATE_CHANGED)
         }, )
+
+        //KakaoSdk 초기화
+        KakaoSdk.init(this, NATIVE_APP_KEY)
+        //디버그용 키 해시 값을 받기 위한 코드 => log에 찍힌 값이 디버그 키
+        //디버그 키는 컴퓨터마다 다르다! 그러므로 개발 팀원 모두의 디버그 키를 각각 등록해줘야 함
+        // 그래서 한번 실행해보시구 찍힌 값 알려주세용!
+        val keyHash = Utility.getKeyHash(this)
+        Log.d("keyHash", "$keyHash") //
     }
     fun getDataStore() = dataStore
 
